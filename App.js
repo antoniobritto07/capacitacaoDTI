5c6b97b62a80bfc599547a1ab94e9d3aecc9efcf
Antonio Britto <antonio.britto.2000@hotmail.com>
2021-04-18 01:12:43 -0300

    Feat: Aula 1 da DTI sobre Native

--- App.js
+++ App.js
@@ -1,12 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  const [inputDate, setInputDate] = useState("");
+  const [inputError, setInputError] = useState("");
+
+  const validateDate = () => {
+    if (isNaN(Date.parse(inputDate)) || inputDate.length !== 10) {
+      setInputError("Digite uma data válida!")
+    }
+    else {
+      setInputError("")
+    }
+  }
+
+  useEffect(() => {
+    validateDate();
+  }, [inputDate])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Digite uma data válida</Text>
+      <TextInput
+        value={inputDate}
+        onChangeText={(value) => { setInputDate(value) }}
+        placeholder={'yyyy-mm-dd'}
+        style={styles.input}
+      />
+      <View style={styles.infoContainer}>
+        <Text>O valor colocado no input é:</Text>
+        <Text>{inputDate}</Text>
+      </View>
+      <Text style={styles.error}>
+        {inputError}
+      </Text>
     </View>
   );
 }
@@ -14,8 +41,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  input: {
+    borderColor: '#000',
+    borderRadius: 15,
+    borderWidth: 2,
+    padding: 10,
+    marginTop: 20
+  },
+  infoContainer: {
+    display: "flex",
+    flexDirection: "column",
+    marginTop: 20,
+    backgroundColor: "rgba(0,0,0,0.3)",
+    padding: 30,
+    borderRadius: 12
+  },
+  error: {
+    fontSize: 15,
+    color: "red",
+    marginTop: 0,
+    padding: 15,
   },
 });
